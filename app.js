541fbff193b0c32f717e806fa1a62bb75a10949d
ellukasion <lukasion@gmail.com>
2022-07-25 21:43:59 +0200

    Init Sequelize

--- app.js
+++ app.js
@@ -1,7 +1,19 @@
 const express = require('express')
 const app = express()
 const port = 3001
-const mysql = require('mysql');
+const connection = require('./config/connection.json');
+const Sequelize = require('sequelize');
+const { DataTypes } = Sequelize;
+
+const sequelize = new Sequelize({
+  database: connection.database,
+  username: connection.user,
+  host: connection.host,
+  port: connection.port,
+  password: connection.password,
+  dialect: 'mysql',
+  operatorsAliases: false
+});
 
 app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
@@ -10,18 +22,6 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Credentials', true);
 
     next();
-});
-
-var con = mysql.createConnection({
-    host: "localhost",
-    user: "root",
-    password: "",
-    database: 'react-api'
-});
-  
-con.connect(function(err) {
-    if (err) throw err;
-    console.log("Connected!");
 });
 
 app.use(express.urlencoded());
